a698fd091628c081f942cdfc573d602d933c38f2
ndichutony@gmail.com <ndichutony@gmail.com>
2020-09-03 00:38:55 +0300

    add image componnets and lightbox

---
+++ src/components/assets/photos.js
@@ -0,0 +1,57 @@
+import imageOne from './carousel-images/img1.jpg';
+import imageTwo from './carousel-images/img2.jpg';
+import imageThree from './carousel-images/img3.jpg';
+import imageFour from './carousel-images/img4.jpg';
+import imageFive from './carousel-images/img5.jpg';
+import imageSix from './carousel-images/img6.jpg';
+import imageSeven from './carousel-images/img7.jpg';
+import imageEight from './carousel-images/img8.jpg';
+import imageNine from './carousel-images/img9.jpg';
+
+export const photos = [
+        {
+            src: imageOne,
+            width: 4,
+            height: 3
+          },
+          {
+            src: imageTwo,
+            width: 1,
+            height: 1
+          },
+          {
+            src: imageThree,
+            width: 3,
+            height: 4
+          },
+          {
+            src: imageFour,
+            width: 3,
+            height: 4
+          },
+          {
+            src: imageFive,
+            width: 3,
+            height: 4
+          },
+          {
+            src: imageSix,
+            width: 4,
+            height: 3
+          },
+          {
+            src: imageSeven,
+            width: 3,
+            height: 4
+          },
+          {
+            src: imageEight,
+            width: 4,
+            height: 3
+          },
+          {
+            src: imageNine,
+            width: 4,
+            height: 3
+          }
+]
--- src/components/videoPlayer.js
+++ src/components/videoPlayer.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactPlayer from 'react-player';
 import campusVideo from '../images/campus-video.mp4';
+import ImageGallery from './imageGallery';
 
 const videoPlayer = () => {
     return (
-        <ReactPlayer url={campusVideo} />
-    )
+        <>
+            <ReactPlayer url={campusVideo} playing={false} controls={true} />
+            <ImageGallery />
+        </>
+    );
 }
 
 export default videoPlayer;
